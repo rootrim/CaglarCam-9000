ce44278b054327fb4f12567b547f4fcdb7b3010b
gitrim <omuaz01@gmail.com>
2025-02-05 19:46:36 +0300

    Removed test lines
    Removed three lines of code for written in test

--- src/detect.py
+++ src/detect.py
@@ -5,7 +5,4 @@ def detectFaces(img):
     face_cascade = CascadeClassifier("models/haarcascade_frontalface_default.xml")
     gray = cvtColor(img, COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    print(type(faces))
-    print(type(faces[0]))
-    print(type(faces[0][0]))
     return faces
