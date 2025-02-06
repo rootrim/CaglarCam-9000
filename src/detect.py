b8e51a04c0c98e370313151a7a90c3e0fbd06f69
gitrim <omuaz01@gmail.com>
2025-02-06 17:15:26 +0300

    Some code changes for car detecting
    Detect generalled

--- src/detect.py
+++ src/detect.py
@@ -1,8 +1,8 @@
 from cv2 import cvtColor, CascadeClassifier, COLOR_BGR2GRAY
 
 
-def detectFaces(img):
-    face_cascade = CascadeClassifier("models/haarcascade_frontalface_default.xml")
+def detectFaces(img, cascade):
+    cascade = CascadeClassifier(cascade)
     gray = cvtColor(img, COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    return faces
+    detecteds = cascade.detectMultiScale(gray, 1.1, 4)
+    return detecteds
